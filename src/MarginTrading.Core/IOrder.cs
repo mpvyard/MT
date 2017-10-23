02262000944193f2c426fdd3f1c4fe17327c3e07
Andrey Anokhin <yashmel@mail.ru>
2017-10-23 18:58:27 +0300

    LWDEV-3559: renamed close reason back

--- src/MarginTrading.Core/IOrder.cs
+++ src/MarginTrading.Core/IOrder.cs
@@ -102,7 +102,7 @@ namespace MarginTrading.Core
         StopLoss,
         TakeProfit,
         StopOut,
-        CanceledByUser,
+        Canceled,
         CanceledBySystem
     }
 
@@ -188,7 +188,7 @@ namespace MarginTrading.Core
                     }
 
                     message = order.ExpectedOpenPrice.HasValue && 
-                              (order.CloseReason == OrderCloseReason.CanceledByUser || 
+                              (order.CloseReason == OrderCloseReason.Canceled || 
                                order.CloseReason == OrderCloseReason.CanceledBySystem)
                         ? string.Format(MtMessages.Notifications_PendingOrderCanceled, type, order.Instrument, volume)
                         : string.Format(MtMessages.Notifications_OrderClosed, type, order.Instrument, volume, reason,
